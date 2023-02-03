e0c6b610bc29af0ad30dc80151e50c31e371742e
hp <hp@DESKTOP-FK14J97>
2023-02-03 10:34:52 +0530

    dk

---
+++ src/corejava/ExcepHand.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class ExcepHand {
+public static void main(String args[]) {
+	
+	try {
+		int a= 10/9;
+		System.out.println(a);
+	}
+	catch(Exception e) {
+		System.out.println(e);
+	}
+}
+}
